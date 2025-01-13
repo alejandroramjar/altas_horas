5121820e8326501ad08359fbc6eee8c8f5ed21fe
alejandroramjar <ramjar2107@gmail.com>
2025-01-13 17:56:52 -0500

    Add sidebar entries for 'Registro Prod' and 'Products' in sidebar-data.ts

--- frontend/src/app/pages/ui-components/ui-components.routes.ts
+++ frontend/src/app/pages/ui-components/ui-components.routes.ts
@@ -1,5 +1,10 @@
 import { Routes } from '@angular/router';
+/*
+En este archivo se define con la siguiente configuracion, cuando se agreag un componente para que este se muestre debe
+ listarse cmo ruta siendo el path la ruta fisica en carpeta y componenet el nombre del componente a renderizar en ese
+ momento, esto garantixza que este sea selecionado
 
+ */
 // ui
 import { AppBadgeComponent } from './badge/badge.component';
 import { AppChipsComponent } from './chips/chips.component';
@@ -8,6 +13,8 @@ import { AppMenuComponent } from './menu/menu.component';
 import { AppTooltipsComponent } from './tooltips/tooltips.component';
 import { AppFormsComponent } from './forms/forms.component';
 import { AppTablesComponent } from './tables/tables.component';
+import { AppProductsTablesComponent} from "./products/products.component";
+import {AppRegitroProductComponent} from "./registrar_producto/registro_product.component";
 
 export const UiComponentsRoutes: Routes = [
   {
@@ -38,8 +45,16 @@ export const UiComponentsRoutes: Routes = [
         component: AppFormsComponent,
       },
       {
+        path: 'registrar_producto',
+        component: AppRegitroProductComponent,
+      },
+      {
         path: 'tables',
         component: AppTablesComponent,
+      },
+      {
+        path: 'products',
+        component: AppProductsTablesComponent,
       },
     ],
   },
