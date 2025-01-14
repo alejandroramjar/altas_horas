0d40621587e73bfda44d5b99a6be5ca65991a551
alejandroramjar <ramjar2107@gmail.com>
2025-01-14 13:51:38 -0500

    Add Usuario model extending User for enhanced user management
    - Introduce `Usuario` model that inherits from `User`
    - Implement `cpf` field with unique constraint
    - Create `phone` field with regex validation for 8-digit phone numbers
    - Define `__str__` method to return full name of the user

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
+from django.core.validators import RegexValidator
 
 # Create your models here.
+
+class Usuario(User):
+    #dispositivos = models.ManyToManyField(Dispositivo, related_name='usuarios', blank=True)
+    cpf = models.CharField(max_length=11, unique=True)
+    phone_regex = RegexValidator(regex=r'^\d{8}$', message="El número de teléfono debe tener exactamente 8 dígitos.")
+    phone = models.CharField(validators=[phone_regex], max_length=8, blank=True, unique=True)
+
+
+    def __str__(self):
+        return self.get_full_name()
